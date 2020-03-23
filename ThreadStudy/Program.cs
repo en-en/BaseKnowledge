864a7863ce057a957cae2c527138b1b9ba92763d
WangGang <wanggang@bestwise.cc>
2020-03-23 09:31:31 +0800

    添加项目文件。

---
+++ ThreadStudy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ThreadStudy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
